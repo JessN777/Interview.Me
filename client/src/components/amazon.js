6ab30b121a0c5f296b196371b019f8d0da47aa97
Brian-Miki <bmiki5508@gmail.com>
2022-12-12 23:48:37 -0800

    Home Page Setup

---
+++ client/src/components/amazon.js
@@ -0,0 +1,7 @@
+import Button from '@mui/material/Button';
+
+const amazon = () => {
+    return <Button>Amazon Card</Button>;
+};
+
+export default amazon;
---
+++ client/src/components/uber.js
@@ -0,0 +1,6 @@
+import Button from '@mui/material/Button';
+const uber = () => {
+    return <Button>Uber Card</Button>;
+};
+
+export default uber;
