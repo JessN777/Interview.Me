25d83cecbd205f816f6f802142b84e53f02ae0d3
Brian-Miki <bmiki5508@gmail.com>
2022-12-14 20:28:41 -0800

    Breadcrumbs

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,57 +1,51 @@
 import React from "react";
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, Breadcrumbs, Link } from "@mui/material";
+
+function handleClick(event) {
+  event.preventDefault();
+  console.info("You clicked a breadcrumb.");
+}
 
 export default function NavBar() {
   return (
-    <div>
-      <AppBar position="static" style={{ backgroundColor: "lightblue" }}>
-        <Toolbar>
-          <Typography
-            variant="h6"
-            style={{
-              flexGrow: 1,
-              textAlign: "center",
-              color: "white",
-              fontWeight: "bold",
-            }}
-          >
-            Type of Company/Role
-          </Typography>
-          <Typography
-            variant="h6"
-            style={{
-              flexGrow: 1,
-              textAlign: "center",
-              color: "white",
-              fontWeight: "bold",
-            }}
-          >
-            Type of Questions
-          </Typography>
-          <Typography
-            variant="h6"
-            style={{
-              flexGrow: 1,
-              textAlign: "center",
-              color: "white",
-              fontWeight: "bold",
-            }}
-          >
-            AI Interview
-          </Typography>
-          <Typography
-            variant="h6"
-            style={{
-              flexGrow: 1,
-              textAlign: "center",
-              color: "white",
-              fontWeight: "bold",
-            }}
-          >
-            Feedback
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    <div role="presentation" onClick={handleClick}>
+      <Breadcrumbs aria-label="breadcrumb">
+        <Link
+          underline="hover"
+          color="inherit"
+          href="https://en.wikipedia.org/wiki/Spider-Man"
+        >
+          Select a Company
+        </Link>
+        <Link
+          underline="hover"
+          color="inherit"
+          href="https://en.wikipedia.org/wiki/Spider-Man"
+        >
+          Select a Role
+        </Link>
+        <Link
+          underline="hover"
+          color="inherit"
+          href="https://en.wikipedia.org/wiki/Spider-Man"
+        >
+          Input Your Questions
+        </Link>
+        <Link
+          underline="hover"
+          color="inherit"
+          href="https://en.wikipedia.org/wiki/Spider-Man"
+        >
+          Practice Your Interview
+        </Link>
+        <Link
+          underline="hover"
+          color="inherit"
+          href="https://en.wikipedia.org/wiki/Spider-Man"
+        >
+          Feedback
+        </Link>
+      </Breadcrumbs>
     </div>
   );
 }
