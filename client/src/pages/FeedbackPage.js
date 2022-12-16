fb5a8fc3e0601691666dc7b1e10ff630d8ddebfa
JessN777 <120295549+JessN777@users.noreply.github.com>
2022-12-15 17:27:31 -0800

    Commit message

--- client/src/pages/FeedbackPage.js
+++ client/src/pages/FeedbackPage.js
@@ -8,8 +8,10 @@ import {
   Grid,
   Button,
   Avatar,
+  Box,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { useGlobalState, postGptCommand } from "../global";
 import { setGlobalState } from "../global";
 
 const FeedbackPage = () => {
@@ -23,23 +25,68 @@ const FeedbackPage = () => {
     navigate("/");
   };
 
+  const gptOutput1 = "Good things to take from the interview";
+  const gptOutput2 = "Things you should improve on";
+
   return (
     <Paper elevation={0}>
-      <Typography variant="h6">Feedback</Typography>
-      <Typography>
+      <Typography variant="h6" style={{ textAlign: "center" }}>
+        Feedback
+      </Typography>
+      <Typography style={{ textAlign: "center" }}>
         Now it is time to understand how you can improve your responses.
       </Typography>
-      <Typography variant="h6">Notes:</Typography>
-      <Avatar alt="Bryan Li" src="/images/Bryan Li.jpg">
-        {"Bryan Li"}
-      </Avatar>
-      <Typography>
-        This will be the spot where the feedback will be presented.
-      </Typography>
+      <Typography variant="h6"> Notes:</Typography>
 
-      {/* Display feedback cards here */}
+      {/* Display the boxes side by side */}
+      <Box
+        sx={{
+          marginTop: 5,
+          display: "flex",
+          width: "100%",
+          height: 200,
+          alignSelf: "center",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 3,
+        }}
+      >
+        {/* Display the first box */}
+        <Box
+          sx={{
+            width: "50%",
+            height: "100%",
+            backgroundColor: "white",
+            alignSelf: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 3,
+            borderColor: "black",
+            borderWidth: 2,
+            borderStyle: "solid",
+          }}
+        >
+          <Typography sx={{ fontWeight: "bold" }}>{gptOutput1}</Typography>
+        </Box>
 
-      <Button onClick={handleNavigateToHome}>Go to Home</Button>
+        {/* Display the second box */}
+        <Box
+          sx={{
+            width: "50%",
+            height: "100%",
+            backgroundColor: "white",
+            alignSelf: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 3,
+            borderColor: "black",
+            borderWidth: 2,
+            borderStyle: "solid",
+          }}
+        >
+          <Typography sx={{ fontWeight: "bold" }}>{gptOutput2}</Typography>
+        </Box>
+      </Box>
     </Paper>
   );
 };
